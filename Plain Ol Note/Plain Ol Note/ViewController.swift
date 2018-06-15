7da091b510922296679a6b30e2b572cb1486b5c8
Rajesh Narayana Rao <mailrbn@gmail.com>
2018-06-16 01:57:54 +1000

    jun

---
+++ Plain Ol Note/Plain Ol Note/ViewController.swift
@@ -0,0 +1,45 @@
+//
+//  ViewController.swift
+//  Plain Ol Note
+//
+//  Created by Rajesh Narayana Rao on 11/6/18.
+//  Copyright © 2018 Rajesh Narayana Rao. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UITableViewDataSource {
+    
+    
+    @IBOutlet weak var table: UITableView!
+    var data:[String] = ["Item 1", "Item 2", "Item 3"]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        table.dataSource = self
+        self.title = "Notes"
+        self.navigationController?.navigationBar.prefersLargeTitles = true
+        let addButton = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(addNote))
+        self.navigationItem.rightBarButtonItem = addButton
+    }
+    
+    @objc func addNote() {
+        let name:String = "Item \(data.count + 1)"
+        data.insert(name, at: 0)
+        let indexPath:IndexPath = IndexPath(row: 0, section: 0)
+        table.insertRows(at: [indexPath], with: .automatic)
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return data.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell:UITableViewCell = tableView.dequeueReusableCell(withIdentifier: "cell")!
+        cell.textLabel?.text = data[indexPath.row]
+        return cell
+    }
+
+}
+
---
+++ Swift4_2ProgrammingLanguage.playground/Contents.swift
@@ -0,0 +1,171 @@
+import UIKit
+
+var str = "Hello, playground"
+print(str)
+var myVariable = 42
+myVariable = 50
+let myConstant = 42
+let implicitInteger = 70
+let implicitDouble = 70.0
+let explicitDouble: Double = 70
+let label = "The width is "
+let width = 94
+let widthLabel = label + String(width)
+let apples = 3
+let oranges = 5
+let appleSummary = "I have \(apples) apples."
+let fruitSummary = "I have \(apples + oranges) pieces of fruit."
+let quotation = """
+I said "I have \(apples) apples."
+And then I said "I have \(apples + oranges)
+    piece of fruit."
+"""
+print(quotation)
+var shoppingList = ["catfish", "water", "tulips", "blue paint"]
+shoppingList[1] = "bottle of water"
+print(shoppingList)
+var occupation = [
+    "Malcolm": "Captain",
+    "Kaylee": "Mechanic",
+]
+occupation["Jayne"] = "Public Relations"
+print(occupation)
+let emptyArray = [String]()
+let emptyDictionary = [String: Float]()
+shoppingList = []
+occupation = [:]
+let individualScores = [75, 43, 103, 87, 12]
+var teamScore = 0
+for score in individualScores {
+    if score > 50 {
+        teamScore += 3
+    } else {
+        teamScore += 1
+    }
+}
+print(teamScore)
+
+var optionalString: String? = "Hello"
+optionalString = nil
+print(optionalString == nil)
+
+var optionalName: String? = "John Appleseed"
+optionalName = nil
+var greeting = "Hello!"
+if let name = optionalName {
+    greeting = "Hello, \(name)"
+}
+print(greeting)
+
+let nickName: String? = nil
+let fullName: String = "John Appleseed"
+let informalGreeting = "Hi \(nickName ?? fullName)"
+
+let vegetable = "red pepper"
+switch vegetable {
+case "celery":
+    print("Add some raisins and make ants on a log.")
+case "cucumber", "watercress":
+    print("That would make a good tea sandwich.")
+case let x where x.hasSuffix("pepper"):
+    print("Is it a spicy \(x)?")
+default:
+    print("Everything tastes good in soup.")
+}
+
+let interestingNumbers = [
+    "Prime": [2, 3, 5, 7, 11, 13, 29],
+    "Fibonacci": [1, 1, 2, 3, 5, 8],
+    "Square": [1, 4, 9, 16, 25],
+]
+var largest = 0
+for (_, numbers) in interestingNumbers {
+    for number in numbers {
+        if number > largest {
+            largest = number
+        }
+    }
+}
+print(largest)
+
+var n = 2
+while n < 100 {
+    n *= 2
+}
+print (n)
+
+var m = 2
+repeat {
+    m *= 2
+} while m < 100
+print (m)
+
+var total = 0
+for i in 0..<4 {
+    total += i
+}
+print(total)
+
+func greet(person: String, day: String) -> String {
+    return "Hello \(person), today is \(day)."
+}
+greet(person: "Bob", day: "Tuesday")
+
+func greet(_ person: String, on day: String) -> String {
+    return "Hello \(person), today is \(day)."
+}
+greet(person: "John", day: "Wednesday")
+
+func calculateStatistics(scores: [Int]) -> (min: Int, max: Int, sum: Int) {
+    var min = scores[0]
+    var max = scores[0]
+    var sum = 0
+    
+    for score in scores {
+        if score > max {
+            max = score
+        } else if score < min {
+            min = score
+        }
+        sum += score
+    }
+    
+    return (min, max, sum)
+}
+let statistics = calculateStatistics(scores: [5, 3, 100, 3, 9])
+print(statistics.sum)
+print(statistics.0)
+
+func returnFifteen() -> Int {
+    var y = 10
+    func add() {
+        y += 5
+    }
+    add()
+    return y
+}
+returnFifteen()
+
+func makeIncrementer() -> ((Int) -> Int) {
+    func addOne(number: Int) -> Int {
+        return 1 + number
+    }
+    return addOne
+}
+
+var increment = makeIncrementer()
+increment(7)
+
+func hasAnyMatches(list: [Int], condition: (Int) -> Bool) -> Bool {
+    for item in list {
+        if condition(item) {
+            return true
+        }
+    }
+    return false
+}
+func lessThanTen(number: Int) -> Bool {
+    return number < 10
+}
+var numbers = [20, 19, 7, 12]
+hasAnyMatches(list: numbers, condition: lessThanTen)
